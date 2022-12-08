de9e5e07f760e856d85eb4be8109775474a38a33
Ezra <ezr.hilal18@gmail.com>
2022-12-08 19:27:43 +0700

    Change Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:register_login/screens/home/admin/admin_dashboard.dart';
+import 'package:register_login/screens/login/pilihan_login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AdminDashboard(),
+      home: PilihanLogin(),
       debugShowCheckedModeBanner: false,
     );
   }
